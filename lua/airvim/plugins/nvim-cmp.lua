ab0ed93ef891c3dc599af07f2dc6b55261900b18
Jordan Lacopo <jordanlacopo@Jordans-Mac-Studio.local>
2023-02-27 13:43:30 -0500

    Added tab to better complete words and enter to full complete

--- lua/airvim/plugins/nvim-cmp.lua
+++ lua/airvim/plugins/nvim-cmp.lua
@@ -1,18 +1,14 @@
-local M = {}
-M.methods = {}
-
+-- import nvim-cmp plugin safely
 local has_words_before = function()
+	unpack = unpack or table.unpack
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-M.methods.has_words_before = has_words_before
 
--- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
 	return
 end
-
 -- import luasnip plugin safely
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
@@ -55,9 +51,7 @@ cmp.setup({
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
 
 		["<C-e>"] = cmp.mapping.abort(),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-f>"] = cmp.mapping(function(fallback)
 			if luasnip.jumpable(1) then
 				luasnip.jump(1)
@@ -74,26 +68,27 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 
-		["<Tab>"] = cmp.mapping(function(fallback)
-			local col = vim.fn.col(".") - 1
-
-			if cmp.visible() then
-				cmp.select_next_item(select_opts)
-			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-				fallback()
-			else
-				cmp.complete()
+		["<Tab>"] = function(fallback)
+			if not cmp.select_next_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
 			end
-		end, { "i", "s" }),
+		end,
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item(select_opts)
-			else
-				fallback()
+		["<S-Tab>"] = function(fallback)
+			if not cmp.select_prev_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
 			end
-		end, { "i", "s" }),
+		end,
 	},
+
 	-- sources for autocompletion
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" }, -- lsp
@@ -101,6 +96,13 @@ cmp.setup({
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),
+	duplicates = {
+		buffer = 1,
+		path = 1,
+		nvim_lsp = 0,
+		luasnip = 1,
+	},
+	duplicates_default = 0,
 	window = {
 		completion = cmp_window.bordered(),
 		documentation = cmp_window.bordered(),
